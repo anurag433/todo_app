0e54db6165d75f767a20bdfcaeddc2187e77d866
Anurag Singh <anurag504singh@gmail.com>
2025-01-09 19:02:23 +0530

    email_delete

--- todo/settings.py
+++ todo/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
+import os
+from dotenv import load_dotenv
+
+ 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,10 +140,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "anurag504singh@gmail.com"
-EMAIL_HOST_PASSWORD = "nkuhrekookguepsr"
+
+
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")        
+EMAIL_HOST = os.getenv("EMAIL_HOST")            
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") 
+EMAIL_PORT = os.getenv("EMAIL_PORT") 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
+
+
+
+
 
