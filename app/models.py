6cbcc6e35175dc52a13805dd43e7f68e6368caa2
Anurag Singh <anurag504singh@gmail.com>
2025-01-10 21:54:56 +0530

    priority

--- app/models.py
+++ app/models.py
@@ -9,19 +9,13 @@ class TODO(models.Model):
     ('P', 'PENDING'),
     ]
     priority_choices = [
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3'),
-    ('4', '4'),
-    ('5', '5'),
-    ('6', '6'),
-    ('7', '7'),
-    ('8', '8'),
-    ('9', '9'),
-    ('10', '10'),
+    ('High', 'High'),
+    ('Medium', 'Medium'),
+    ('Low', 'Low'),
+   
     ]
     title = models.CharField(max_length=50)
     status = models.CharField(max_length=2 , choices=status_choices)
     user  = models.ForeignKey(User  , on_delete= models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    priority = models.CharField(max_length=2 , choices=priority_choices)
+    priority = models.CharField(max_length=10 , choices=priority_choices)
