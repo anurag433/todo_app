2eb411731135c926f73da6079b86c69d1e0ba13c
Anurag Singh <anurag504singh@gmail.com>
2025-02-07 16:38:07 +0530

    update

--- app/views.py
+++ app/views.py
@@ -115,7 +115,7 @@ def add(request):
 def delete(request , id ):
     print(id)
     todo = TODO.objects.get(pk = id )
-    first_name =todo.user.first_name , # beleow this  you may delete 
+    first_name =todo.user.first_name  # beleow this  you may delete 
     email = todo.user.email
     todo.delete()
     # first_name =todo.user.first_name ,
